ded41b02814b56d42d89b3fb7b04bf465559df1f
Ahm6dalii <ahmedalielian20@gmail.com>
2025-02-06 06:24:58 +0200

    fix path import layout

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import Home from '../home/home'
+import Home from '../home/Home'
 import { changeMode } from '../../redux/reducers/modeSlice'
 import Toast from '../../components/toast/toast'
 
